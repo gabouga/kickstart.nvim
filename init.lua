ec6db7b74b0262b31aa59d24f878324c952b7cdb
Francis <francis@physiotec.org>
2023-06-09 16:47:49 -0400

    configure illuminate plugin, null-ls and prettier

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -31,6 +34,7 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
+  -- plugin to enable :GBrowse with bitbucket
   'tommcdo/vim-fubitive',
 
   -- Detect tabstop and shiftwidth automatically
@@ -95,6 +99,9 @@ require('lazy').setup({
       },
     },
   },
+  -- Highlight references using LSP, regex
+  'RRethy/vim-illuminate',
+  'jose-elias-alvarez/null-ls.nvim',
 
   {
     -- Theme inspired by Atom
@@ -431,10 +438,10 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 
   -- The line below enables the text highlight of the cursor
-  vim.cmd [[autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()]]
-  vim.cmd [[autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()]]
-  vim.cmd [[autocmd CursorMovedI <buffer> lua vim.lsp.buf.clear_references()]]
-  vim.cmd [[autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()]]
+  -- vim.cmd [[autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()]]
+  -- vim.cmd [[autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()]]
+  -- vim.cmd [[autocmd CursorMovedI <buffer> lua vim.lsp.buf.clear_references()]]
+  -- vim.cmd [[autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()]]
 end
 
 -- Enable the following language servers
@@ -558,6 +565,72 @@ cmp.setup.cmdline(':', {
   }, {
     { name = 'cmdline' }
   })
+})
+
+
+-- default configuration for vim-illuminate
+require('illuminate').configure({
+  -- providers: provider used to get references in the buffer, ordered by priority
+  providers = {
+    'lsp',
+    'treesitter',
+    'regex',
+  },
+  -- delay: delay in milliseconds
+  delay = 100,
+  -- filetype_overrides: filetype specific overrides.
+  -- The keys are strings to represent the filetype while the values are tables that
+  -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+  filetype_overrides = {},
+  -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+  filetypes_denylist = {
+    'dirvish',
+    'fugitive',
+  },
+  -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
+  filetypes_allowlist = {},
+  -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+  -- See `:help mode()` for possible values
+  modes_denylist = {},
+  -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
+  -- See `:help mode()` for possible values
+  modes_allowlist = {},
+  -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+  -- Only applies to the 'regex' provider
+  -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+  providers_regex_syntax_denylist = {},
+  -- providers_regex_syntax_allowlist: syntax to illuminate, this is overriden by providers_regex_syntax_denylist
+  -- Only applies to the 'regex' provider
+  -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+  providers_regex_syntax_allowlist = {},
+  -- under_cursor: whether or not to illuminate under the cursor
+  under_cursor = true,
+  -- large_file_cutoff: number of lines at which to use large_file_config
+  -- The `under_cursor` option is disabled when this cutoff is hit
+  large_file_cutoff = nil,
+  -- large_file_config: config to use for large files (based on large_file_cutoff).
+  -- Supports the same keys passed to .configure
+  -- If nil, vim-illuminate will be disabled for large files.
+  large_file_overrides = nil,
+  -- min_count_to_highlight: minimum number of matches required to perform highlighting
+  min_count_to_highlight = 2,
+})
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+
+-- Configure null-ls lsp with eslint_d and prettierd
+local status, null_ls = pcall(require, "null-ls")
+if (not status) then return end
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
+    }),
+    null_ls.builtins.diagnostics.fish,
+    null_ls.builtins.formatting.prettierd
+  }
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
